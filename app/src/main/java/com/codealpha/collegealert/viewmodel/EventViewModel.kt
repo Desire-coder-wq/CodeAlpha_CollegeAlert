feb24558ea0882f03a3f36e40a5407b448cf16ff
Desire <asinguradesirecomfort@gmail.com>
2026-05-10 12:52:53 +0300

    add the buttons functionality

--- app/src/main/java/com/codealpha/collegealert/viewmodel/EventViewModel.kt
+++ app/src/main/java/com/codealpha/collegealert/viewmodel/EventViewModel.kt
@@ -13,19 +13,34 @@ class EventViewModel(private val repository: EventRepository = EventRepository()
     private val _events = mutableStateOf<List<Event>>(emptyList())
     val events: State<List<Event>> = _events
 
-    private val _isLoading = mutableStateOf(true)
+    private val _isLoading = mutableStateOf(false)
     val isLoading: State<Boolean> = _isLoading
 
-    init {
-        fetchEvents()
-    }
+    private val _createEventSuccess = mutableStateOf(false)
+    val createEventSuccess: State<Boolean> = _createEventSuccess
 
-    private fun fetchEvents() {
+    fun fetchEvents(collegeId: String) {
         viewModelScope.launch {
-            repository.getEvents().collect { eventList ->
+            _isLoading.value = true
+            repository.getEvents(collegeId).collect { eventList ->
                 _events.value = eventList
                 _isLoading.value = false
             }
         }
+    }
+
+    fun createEvent(event: Event) {
+        viewModelScope.launch {
+            _isLoading.value = true
+            val result = repository.createEvent(event)
+            if (result.isSuccess) {
+                _createEventSuccess.value = true
+            }
+            _isLoading.value = false
+        }
+    }
+
+    fun resetCreateSuccess() {
+        _createEventSuccess.value = false
     }
 }
