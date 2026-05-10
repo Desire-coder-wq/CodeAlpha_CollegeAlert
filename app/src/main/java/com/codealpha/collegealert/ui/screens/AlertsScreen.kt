5ca0978623749d5240febcff74cb3ea23eb1c1ca
Desire <asinguradesirecomfort@gmail.com>
2026-05-10 11:37:37 +0300

    intergrate alert screen

--- app/src/main/java/com/codealpha/collegealert/ui/screens/AlertsScreen.kt
+++ app/src/main/java/com/codealpha/collegealert/ui/screens/AlertsScreen.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.NotificationsActive
 import androidx.compose.material.icons.filled.Search
@@ -17,6 +16,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.codealpha.collegealert.data.model.Event
+import com.codealpha.collegealert.ui.components.AlertCard
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
